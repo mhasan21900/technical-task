2eb8a5617c05d8bc22e77ed67a0a571c9c6b71dd
Muhammad Hasan <142748479+mhasan21900@users.noreply.github.com>
2023-09-09 02:31:27 -0400

    Create tenToOne.js

---
+++ tenToOne.js
@@ -0,0 +1,12 @@
+//Write a program that can print from 10 to 1 numbers
+
+/*
+Initial Value of n is set to 10
+Each iteration checks if n greater than 0
+then substracts 1 from original value of n
+Outputs the value of n in each iteration
+*/
+
+for ( n = 10; n > 0; n--) {
+   console.log(n);
+}
